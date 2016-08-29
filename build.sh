dfcfc787caa783c229868ce2c1dd44fa99262e92
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-08-29 18:17:16 +0200

    build.sh now returns non-zero exit code if pyrcc5 fails

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-pyrcc5 -o aw_qt/resources.py aw_qt/resources.qrc
+function fail() {
+    echo "Failed running last command"
+    exit 1
+}
+
+echo "Running pyrcc5 to build resources.py file"
+pyrcc5 -o aw_qt/resources.py aw_qt/resources.qrc || fail
