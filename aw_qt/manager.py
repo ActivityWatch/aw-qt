b7ca0632b7bc5ebbdda9d22bdfa649b568585387
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-09-04 19:54:19 +0200

    WIP on async module manager

--- aw_qt/manager.py
+++ aw_qt/manager.py
@@ -1,8 +1,11 @@
 import os
 from time import sleep
 import logging
-import subprocess
-from subprocess import PIPE
+from contextlib import closing
+
+import asyncio
+from asyncio import subprocess
+from asyncio.subprocess import PIPE
 
 from .gui.logviewer import LogViewer
 
@@ -10,7 +13,13 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("aw.qt.manager")
 
 
-class Module:
+class _Module:
+    """
+    Manages a subprocess using asyncio
+
+    https://stackoverflow.com/posts/20697159/revisions
+    """
+
     def __init__(self, name):
         self.name = name
         self.started = False
@@ -19,35 +28,50 @@ class Module:
         self._log = ""
 
     def start(self, testing=False):
-        logger.info("Starting module {}".format(self.name))
+        async def _start():
+            logger.info("Starting module {}".format(self.name))
 
-        # Will start module from localdir if present there,
-        # otherwise will try to call what is available in PATH.
-        exec_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), self.name)
-        exec_cmd = (exec_path if os.path.isfile(exec_path) else self.name) + (" --testing" if testing else "")
-        self._process = subprocess.Popen(exec_cmd, universal_newlines=True,
-                                         stdout=PIPE, stderr=PIPE)
-        self.started = True
+            # Will start module from localdir if present there,
+            # otherwise will try to call what is available in PATH.
+            exec_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), self.name)
+            if os.path.isfile(exec_path):
+                # Executable for module was found in same dir as aw-qt, use it
+                cmd = exec_path
+            else:
+                # Executable for module was not in same dir as aw-qt, use executable in PATH
+                cmd = self.name
+
+            # Args to executable
+            args = " --testing" if testing else ""
+
+            # Start the process
+            exec_cmd = " ".join([cmd, args]).strip()
+            self._process = await asyncio.create_subprocess_exec(exec_cmd, universal_newlines=True,
+                                                                 stdout=PIPE, stderr=PIPE)
+            self.started = True
+
+        loop.call_soon_threadsafe(_start)
 
     def stop(self):
-        """
-        Stops a module, and waits until it terminates.
-        """
-        # TODO: What if a module doesn't stop? Add timeout to p.wait() and then do a p.kill() if timeout is hit
-        if not self.started:
-            logger.warning("Tried to kill module {}, but it has never been started".format(self.name))
-        elif not self.is_alive():
-            logger.warning("Tried to kill module {}, but it was already dead".format(self.name))
-        else:
-            logger.info("Stopping module {}".format(self.name))
-            self._process.terminate()
-            logger.info("Waiting for module {} to shut down".format(self.name))
-            self._process.wait()
-            logger.info("Module {} has shut down".format(self.name))
-        assert not self.is_alive()
-        self._last_process = self._process
-        self._process = None
-        self.started = False
+        # Stops a module, and waits asynchronously until it terminates.
+        async def _stop():
+            # TODO: What if a module doesn't stop? Add timeout to p.wait() and then do a p.kill() if timeout is hit
+            if not self.started:
+                logger.warning("Tried to kill module {}, but it has never been started".format(self.name))
+            elif not self.is_alive():
+                logger.warning("Tried to kill module {}, but it was already dead".format(self.name))
+            else:
+                logger.info("Stopping module {}".format(self.name))
+                self._process.terminate()
+                logger.info("Waiting for module {} to shut down".format(self.name))
+                await self._process.wait()
+                logger.info("Module {} has shut down".format(self.name))
+            assert not self.is_alive()
+            self._last_process = self._process
+            self._process = None
+            self.started = False
+
+        loop.call_soon_threadsafe(_stop)
 
     def is_alive(self):
         if not self.started:
@@ -85,11 +109,38 @@ _possible_modules = [
 ]
 
 # TODO: Filter away all modules not available on system
-modules = {name: Module(name) for name in _possible_modules}
+modules = {name: _Module(name) for name in _possible_modules}
 
 
 def get_unexpected_stops():
     return list(filter(lambda x: x.started and not x.is_alive(), modules.values()))
+
+
+@blocking
+def run():
+    # NOTE: To handle signals and to execute subprocesses, the event loop must be run in the main thread.
+    # https://docs.python.org/3/library/asyncio-dev.html#concurrency-and-multithreading
+    assert threading.current_thread() == threading.main_thread()
+
+    global loop
+    if sys.platform == "win32":
+        loop = asyncio.ProactorEventLoop()
+        asyncio.set_event_loop(loop)
+    else:
+        loop = asyncio.get_event_loop()
+
+    with closing(loop):
+        loop.run_forever()
+    logging.error("Event loop stopped, something must have went wrong")
+
+
+def start_module(name):
+    modules[name].start()
+
+
+def stop_module(name):
+    modules[name].stop()
+
 
 if __name__ == "__main__":
     for module in modules.values():
