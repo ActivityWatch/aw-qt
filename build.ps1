910a8b9e4afb908b04fea989d6c077c8b6125bb7
Erik Bjäreholt <erik.bjareholt@gmail.com>
2017-04-11 13:04:23 +0200

    Updated appdirs version constraint

--- build.ps1
+++
@@ -1,6 +0,0 @@
-echo "Building resources.py"
-
-# TODO: We need to find the pyrcc5 path somehow
-C:\Users\erb\AppData\Roaming\Python\Python35\Scripts\pyrcc5.exe aw_qt/resources.qrc -o aw_qt/resources.py
-
-echo "Done building resources.py"
