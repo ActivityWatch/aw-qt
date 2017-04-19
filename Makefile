32e8eb7d6f40a270d99cf766400f66c9fcf12d16
Erik Bjäreholt <erik@bjareho.lt>
2017-04-19 10:34:11 +0200

    added Makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+.PHONY: build
+
+build:
+	./scripts/build.sh
+
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-function fail() {
-    echo "Failed running last command"
-    exit 1
-}
+set -e
 
 echo "Running pyrcc5 to build resources.py file"
 pyrcc5 -o aw_qt/resources.py aw_qt/resources.qrc || fail
+echo "Build successful!"
