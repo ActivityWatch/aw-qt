7b5009040fd8e63f1438cfdc6529f4a74fb6c403
Erik Bjäreholt <erik@bjareho.lt>
2017-08-23 18:16:26 +0200

    added restart button and did some refactoring

--- aw_qt/main.py
+++ aw_qt/main.py
@@ -1,49 +1,25 @@
 import sys
 import logging
 import argparse
-from time import sleep
-from typing import List
 
 from aw_core.log import setup_logging
 
-from . import manager
+from .manager import Manager
+
 from . import trayicon
 
 logger = logging.getLogger(__name__)
-
-
-def autostart(modules: List[str], testing: bool):
-    # Start aw-server and wait for it to boot
-    # FIXME: The wait isn't required if clients can handle an unavailable server at startup
-    if "aw-server" in modules:
-        manager.modules["aw-server"].start(testing=testing)
-        modules.remove("aw-server")
-        sleep(1)
-
-    # Autostart modules
-    autostart_modules = modules
-    for module_name in autostart_modules:
-        if module_name in manager.modules:
-            manager.modules[module_name].start(testing=testing)
-        else:
-            logger.error("Module {} not available".format(module_name))
-
-
-def stop():
-    # TODO: This might no longer be needed due to signal handling etc. in trayicon.py
-    for module in filter(lambda m: m.is_alive(), manager.modules.values()):
-        module.stop()
 
 
 def main():
     args = parse_args()
     setup_logging("aw-qt", testing=args.testing, verbose=args.testing, log_file=True)
 
-    autostart(args.autostart_modules, testing=args.testing)
+    _manager = Manager(testing=args.testing)
+    _manager.autostart()
 
-    error_code = trayicon.run(testing=args.testing)
-
-    stop()
+    error_code = trayicon.run(_manager, testing=args.testing)
+    _manager.stop()
 
     sys.exit(error_code)
 
