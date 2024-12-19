d0e49ee516d3afab1cf452f777b411e9e3d84156
brayo <vukubrian@gmail.com>
2024-12-19 22:36:47 +0300

    feat: set aw-server-rust in default config

--- aw_qt/config.py
+++ aw_qt/config.py
@@ -5,10 +5,10 @@ from aw_core.config import load_config_toml
 
 default_config = """
 [aw-qt]
-autostart_modules = ["aw-server", "aw-watcher-afk", "aw-watcher-window"]
+autostart_modules = ["aw-server-rust", "aw-watcher-afk", "aw-watcher-window"]
 
 [aw-qt-testing]
-autostart_modules = ["aw-server", "aw-watcher-afk", "aw-watcher-window"]
+autostart_modules = ["aw-server-rust", "aw-watcher-afk", "aw-watcher-window"]
 """.strip()
 
 
